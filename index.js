dcf0ddb53d0939fb62e4fb04edd79335bd331248
stellamartinscrp <stella@edujob.comls>
2024-01-05 11:47:23 -0300

    set authentication endpoints and database migrations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const taskRoutes = require('./routes/task')
+const authRoutes = require('./routes/auth')
 const cors = require('cors')
 
 const app = express()
@@ -8,7 +9,8 @@ const PORT = 3000
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/api', taskRoutes)
+app.use('/tasks', taskRoutes)
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`)
--- models/user.js
+++ models/user.js
@@ -3,22 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Task extends Model {
+  class User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Task.belongsTo(User, { foreignKey: 'user_id' });
+      User.hasMany(Task, { foreignKey: 'user_id' });
     }
   }
-  Task.init({
-    title: DataTypes.STRING,
-    completed: DataTypes.BOOLEAN
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Task',
+    modelName: 'User',
   });
-  return Task;
+  return User;
 };
