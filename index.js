e201e25921391597827af897a46f20c787da1117
stellamartinscrp <stella@edujob.comls>
2024-01-05 09:56:53 -0300

    Set cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const taskRoutes = require('./routes/task')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', taskRoutes)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port http://localhost:${PORT}`)
 })
